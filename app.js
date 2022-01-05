a9ac0a7d9282ba4fe7c5c2ab9182f694a2ff8730
Epiq122 <l2ob122@gmail.com>
2022-01-04 19:26:55 -0800

    finished basic game

---
+++ app.js
@@ -0,0 +1,47 @@
+const computerPlay = () => {
+  // these are our 3 options for the game
+  const options = ['rock', 'paper', 'scissors'];
+  // randomlly selects of the options
+  const randomNumber = Math.floor(Math.random() * 3);
+  // returns the random option
+  return options[randomNumber];
+};
+
+const playRound = (playerSelection, computerSelection) => {
+  // this is where we compare the player and computer selection
+  if (playerSelection === computerSelection) {
+    return 'It is a tie!';
+  } else if (playerSelection === 'rock') {
+    if (computerSelection === 'paper') {
+      return 'You lose! Paper beats rock.';
+    } else {
+      return 'You win! Rock beats scissors.';
+    }
+  } else if (playerSelection === 'paper') {
+    if (computerSelection === 'scissors') {
+      return 'You lose! Scissors beats paper.';
+    } else {
+      return 'You win! Paper beats rock.';
+    }
+  } else if (playerSelection === 'scissors') {
+    if (computerSelection === 'rock') {
+      return 'You lose! Rock beats scissors.';
+    } else {
+      return 'You win! Scissors beats paper.';
+    }
+  }
+};
+
+const game = () => {
+  // this is where we play the game
+  // this is where we get the player selection
+  const playerSelection = prompt('Please select rock, paper or scissors.');
+  // this is where we get the computer selection
+  const computerSelection = computerPlay();
+  // this is where we display the result
+  alert(playRound(playerSelection, computerSelection));
+};
+
+const playerSelection = prompt('Rock, Paper, or Scissors?');
+const computerSelection = computerPlay();
+alert(playRound(playerSelection, computerSelection));
